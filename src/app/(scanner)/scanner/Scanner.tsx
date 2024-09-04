a6ef648e2d7e2c040eefc6170c35af2b6c02d9ff
John <noemito211@gmail.com>
2024-09-04 15:41:04 +0800

    added gate personnel

--- src/app/(scanner)/scanner/Scanner.tsx
+++ src/app/(scanner)/scanner/Scanner.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { ImageOff } from 'lucide-react';
+import { ImageOff, ScanLine } from 'lucide-react';
 import React, { useEffect, useRef, useState } from 'react'
 import toast from 'react-hot-toast';
 
@@ -27,6 +27,8 @@ export default function Scanner() {
   const [historyTimeIn, setHistoryTimeIn] = useState<TAttendanceHistory[]>();
   const [historyTimeOut, setHistoryTimeOut] = useState<TAttendanceHistory[]>();
   
+  let timer : NodeJS.Timeout | undefined = undefined
+  
   const handleBodyClick = () => { 
     if(inputScanRef.current) {
       inputScanRef.current.focus()
@@ -39,8 +41,8 @@ export default function Scanner() {
       inputScanRef.current.focus()
     }
 
-    // document.body.addEventListener('keydown', handleKeyDown);
     document.body.addEventListener('click', handleBodyClick)
+    
     return () => {
       // document.body.removeEventListener('keydown', handleKeyDown);
       document.body.removeEventListener('click', handleBodyClick)
@@ -50,10 +52,10 @@ export default function Scanner() {
 
 
   const handleKeyDown = async (event : React.KeyboardEvent<HTMLInputElement>) => {
-
-    if (event.key === 'Enter') {
-      console.log(42, scannerCode)
-
+    
+    if(event.key === 'Enter') {
+      clearTimeout(timer)
+    
       const request = await fetch('api/student/attendance',{
         method: 'POST',
         body: JSON.stringify({code : scannerCode, date: new Date()})
@@ -78,7 +80,8 @@ export default function Scanner() {
       setHistoryTimeIn(timeIn)
       setHistoryTimeOut(timeOut)
       setStudentInfo(response.data)
-
+      
+      timer = setTimeout(() => setStudentInfo(undefined), 1000 * 10)
     }
   }
 
@@ -87,64 +90,74 @@ export default function Scanner() {
       <input ref={inputScanRef} className='absolute left-[-9999px]' value={scannerCode} onChange={(e) => setScannerCode(e.target.value) 
         } onKeyDown={handleKeyDown}/>
       <div className='grid grid-cols-3 h-full gap-4 '>
-        <div className='row-span-4'>
-          {studentInfo?.profile_url ? 
-            ( <img className='h-full' src={studentInfo?.profile_url} alt=""  /> )
-          :
-            (
-              <div className='h-full bg-zinc-600 flex justify-center items-center'>
-                <ImageOff className='text-white' size={60}/>
+        <div className='col-span-3 text-center text-4xl  font-extrabold ' >GSC SPED INTEGRATED SCHOOL</div>
+          {!studentInfo && (
+          <div className='col-span-3 flex justify-center'>
+            <p className='text-2xl font-extrabold uppercase'>Scan your Qrcode</p>
+          </div>
+        )}
+        {studentInfo && (
+          <>
+            <div className='row-span-4'>
+              {studentInfo?.profile_url ? 
+                ( <img className='h-full' src={studentInfo?.profile_url} alt=""  /> )
+              :
+                (
+                  <div className='h-full bg-zinc-600 flex justify-center items-center'>
+                    <ImageOff className='text-white' size={60}/>
+                  </div>
+                )}
+            </div>
+            <div className= 'col-span-2 row-span-4'>
+              <div className='grid grid-cols-2 gap-5'>
+                <div className='col-span-2 flex flex-col justify-end '>
+                  <p className='text-2xl font-bold font-sans   text-slate-500 uppercase'>Full Name</p>
+                </div>
+                <div className='col-span-2  '>
+                  <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.full_name}</p>
+                </div>
+                <div className='col-span-2 flex flex-col justify-end '>
+                  <p className='text-2xl font-bold font-sans  text-slate-500 uppercase'>Grade Level</p>
+                </div>
+                <div className='col-span-2  '>
+                  <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.grade_level}</p>
+                </div>
+                <div className='col-span-2 flex flex-col justify-end '>
+                  <p className='text-2xl font-bold font-sans   text-slate-500 uppercase'>Section</p>
+                </div>
+                <div className='col-span-2  '>
+                  <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.section_name}</p>
+                </div>
+                <div className='col-span-2 flex flex-col justify-end '>
+                  <p className='text-2xl font-bold font-sans   text-slate-500 uppercase'>School year</p>
+                </div>
+                <div className='col-span-2  '>
+                  <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.school_year}</p>
+                </div>
               </div>
-            )}
-        </div>
-        <div className= 'col-span-2 row-span-4'>
-          <div className='grid grid-cols-2 gap-5'>
-            <div className='col-span-2 flex flex-col justify-end '>
-              <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.full_name}</p>
             </div>
-            <div className='col-span-2  p-1'>
-              <p className='text-2xl font-bold font-sans   text-slate-500 uppercase'>Full Name</p>
+            <div className='row-span-5 '>
+              <div className='grid grid-cols-2 '>
+                <div >
+                  <p className='text-2xl font-sans text-center text-slate-500  font-extrabold uppercase'>TIME IN</p>
+                  {historyTimeIn && historyTimeIn.map(v => (
+                    <p key={v.time_in} className='text-2xl font-sans text-center text-slate-900 font-extrabold uppercase'>
+                      {new Intl.DateTimeFormat('en-US', { timeStyle: 'short'}).format(new Date(v.date_time_stamp))}
+                    </p>
+                  ))}
+                </div>
+                <div> 
+                  <p className='text-2xl font-sans text-center text-slate-500  font-extrabold uppercase'>TIME OUT</p>
+                  {historyTimeOut && historyTimeOut.map(v => (
+                    <p key={v.time_out} className='text-2xl font-sans text-center text-slate-900 font-extrabold uppercase'>
+                      {new Intl.DateTimeFormat('en-US', { timeStyle: 'short'}).format(new Date(v.date_time_stamp))}
+                    </p>
+                  ))}
+                </div>    
+              </div>
             </div>
-            <div className='col-span-2 flex flex-col justify-end '>
-              <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.grade_level}</p>
-            </div>
-            <div className='col-span-2  p-1'>
-              <p className='text-2xl font-bold font-sans  text-slate-500 uppercase'>Grade Level</p>
-            </div>
-            <div className='col-span-2 flex flex-col justify-end '>
-              <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.section_name}</p>
-            </div>
-            <div className='col-span-2  p-1'>
-              <p className='text-2xl font-bold font-sans   text-slate-500 uppercase'>Section</p>
-            </div>
-            <div className='col-span-2 flex flex-col justify-end '>
-              <p className='text-4xl   font-sans text-slate-900 font-extrabold uppercase'>{studentInfo?.school_year}</p>
-            </div>
-            <div className='col-span-2  p-1'>
-              <p className='text-2xl font-bold font-sans   text-slate-500 uppercase'>School year</p>
-            </div>
-          </div>
-        </div>
-        <div className='row-span-6 '>
-          <div className='grid grid-cols-2 '>
-            <div >
-              <p className='text-2xl font-sans text-center text-slate-500  font-extrabold uppercase'>TIME IN</p>
-              {historyTimeIn && historyTimeIn.map(v => (
-                <p key={v.time_in} className='text-2xl font-sans text-center text-slate-900 font-extrabold uppercase'>
-                  {new Intl.DateTimeFormat('en-US', { timeStyle: 'short'}).format(new Date(v.date_time_stamp))}
-                </p>
-              ))}
-            </div>
-            <div> 
-              <p className='text-2xl font-sans text-center text-slate-500  font-extrabold uppercase'>TIME OUT</p>
-              {historyTimeOut && historyTimeOut.map(v => (
-                <p key={v.time_out} className='text-2xl font-sans text-center text-slate-900 font-extrabold uppercase'>
-                  {new Intl.DateTimeFormat('en-US', { timeStyle: 'short'}).format(new Date(v.date_time_stamp))}
-                </p>
-              ))}
-            </div>    
-          </div>
-        </div>
+          </>
+        )}
       </div>
       
     </>
--- src/middleware.ts
+++ src/middleware.ts
@@ -13,22 +13,32 @@ export default async function middleware(req: NextRequest) {
   const user = await decrypt(cookie)
   
   const isPublicRoute = publicRoutes.includes(path)
-  
+  // public routes
   if(!user && !isPublicRoute) {
     return NextResponse.redirect(new URL('/login', req.nextUrl))
   }
-  
+  // if user is not aprroved
   if(user && user.approve == false && !req.nextUrl.pathname.startsWith('/approval')) { // if user is not approve yet
     return NextResponse.redirect(new URL('/approval', req.nextUrl))
   }
-  
+  // if user is  aprroved
   if(user && user.approve == true && req.nextUrl.pathname.startsWith('/approval')) {
     return NextResponse.redirect(new URL('/', req.nextUrl))
   }
-  
-  if(user && isPublicRoute) {
+  // gate pesonnel role  
+  if(user && user.role === 'gate_personnel' && !req.nextUrl.pathname.startsWith('/scanner') ) {
+    return NextResponse.redirect(new URL('/scanner', req.nextUrl))
+  }
+
+  // redirect teacher if in public [/login, /sign-up] route
+  if(user && isPublicRoute && user.role === 'teacher') {
     return NextResponse.redirect(new URL('/', req.nextUrl))
   }
+  // redirect teacher if in /scanner route
+  if(user && user.role === 'teacher' && req.nextUrl.pathname.startsWith('/scanner')) {
+    return NextResponse.redirect(new URL('/', req.nextUrl))
+  }
+  
 
   return NextResponse.next()
 }
